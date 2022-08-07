4600f4a9c781dace39cc7e643261bb4d3edf2c67
patil-gau <patilgautam728@gmail.com>
2022-08-08 00:31:26 +0530

    backend complated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const getMetaData = require('metadata-scraper')
 const cors = require('cors');
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 //create  a app
 const app = express();
